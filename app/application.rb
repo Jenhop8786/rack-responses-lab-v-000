ac8c83f549ac19e060d22882f0ca760978e62bcc
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-09 16:25:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,13 @@
-class Application 
+class Application
 
   def call(envy)
     resp = Rack::Response.new
 
     if Time.now.hour < 12
       resp.write "Good Morning"
-    else 
+    else
       resp.write "Good Afternoon"
-    end 
+    end
     resp.finish
-  end 
+  end
 end 
